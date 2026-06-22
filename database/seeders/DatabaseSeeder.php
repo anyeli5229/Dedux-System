573a0fc58ce2611da9a0db157c907fe2b8d331bf
Anyeli Ramos <jessiramos667@gmail.com>
2026-06-22 14:24:30 -0600

    fix: saltar modelo de cashier usando db table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
         $user = User::updateOrCreate(
             ['email' => 'demo@dedux.com'],
             [
@@ -21,16 +23,22 @@ class DatabaseSeeder extends Seeder
                 'email_verified_at' => now(),
             ]
         );
+        DB::statement('ALTER TABLE subscriptions ADD COLUMN IF NOT EXISTS name VARCHAR(255) DEFAULT \'main\';');
 
-        $user->subscriptions()->updateOrCreate(
-            ['name' => 'default'],
+        DB::table('subscriptions')->updateOrInsert(
             [
+                'user_id' => $user->id,
+            ],
+            [
+                'name' => 'default',
                 'stripe_id' => 'sub_demo_123456789',
-                'stripe_status' => 'active', 
+                'stripe_status' => 'active',
                 'stripe_price' => 'price_1TlCeUCj1bk7BciWjC2LZk5h',
                 'quantity' => 1,
                 'trial_ends_at' => null,
                 'ends_at' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
             ]
         );
     }
